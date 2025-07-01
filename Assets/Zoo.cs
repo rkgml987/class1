c1b4c5c44a871c11a022330213b9422f3cd8a66d
DESKTOP-LSH1T0T\202_16 <rkgml987@jnu.ac.kr>
2025-07-01 15:35:19 +0900

    코딩수업을 했습니다

---
+++ Assets/Zoo.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class Zoo : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Animal tom = new Animal();
+        tom.name = "톰";
+        tom.sound = "냐옹!";
+
+        Animal jerry = new Animal();
+        jerry.name = "제리";
+        jerry.sound = "찍찍!";
+
+        tom.PlaySound();
+        jerry.PlaySound();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
